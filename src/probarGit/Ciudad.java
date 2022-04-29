c27e4cdc46d9fdeefcb32c69bfb91836fb756e2d
Stiliyan Yuliyanov Ivanov <stilyanyi@alumnos.iesgalileo.es>
2022-04-29 16:25:22 +0200

    Primera version.

---
+++ src/probarGit/Ciudad.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package probarGit;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ciudad {
+    //Atributos
+    private String nombre;
+    private int poblacion;
+    
+    //Constructor
+    public Ciudad(String nombre, int poblacion) {
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+    }
+
+    public String toString() {
+        return nombre;
+    }
+    
+    
+    
+}
